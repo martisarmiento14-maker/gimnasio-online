d8f64577af5070f88e6f80779a7298eb87213c1f
martisarmiento14-maker <TU_EMAIL_DE_GITHUB>
2025-12-01 02:22:39 -0300

    fix: login funcionando con PostgreSQL

--- routes/cuotas.js
+++ routes/cuotas.js
@@ -1,6 +1,7 @@
-const express = require("express");
+import express from "express";
+import pool from "../db.js";
+
 const router = express.Router();
-const pool = require("../db");
 
 // ==============================
 // LISTAR TODAS LAS CUOTAS
@@ -59,4 +60,4 @@ router.post("/", async (req, res) => {
     }
 });
 
-module.exports = router;
+export default router;
