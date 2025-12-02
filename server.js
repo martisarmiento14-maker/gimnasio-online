98c94f6da8e46fcc355062c34442b9aafbb9f3fd
martisarmiento14-maker <TU_EMAIL_DE_GITHUB>
2025-12-02 12:20:13 -0300

    Actualizo cambios del proyecto

--- server.js
+++ server.js
@@ -1,41 +1,16 @@
 import express from "express";
-console.log("Backend actualizado para redeploy");
-
 import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
-
-import db from "./database/db.js";
-
-import loginRoutes from "./routes/login.js";
-import alumnosRoutes from "./routes/alumnos.js";
 import adminRoutes from "./routes/admin.js";
-import asistenciasRoutes from "./routes/asistencias.js";
+import alumnosRoutes from "./routes/alumnos.js";
 
 const app = express();
 
-// 🚨 CORS DEFINITIVO
-app.use(cors({
-    origin: [
-        "https://gimnasio-online-frontend.onrender.com",
-        "https://gimnasio-online-1.onrender.com",
-        "http://localhost:3000",
-        "http://127.0.0.1:5501"   // <--- AGREGADO PARA LIVE SERVER
-    ],
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
-// Rutas
-app.use("/login", loginRoutes);
-app.use("/alumnos", alumnosRoutes);
 app.use("/admin", adminRoutes);
-app.use("/asistencias", asistenciasRoutes);
+app.use("/alumnos", alumnosRoutes);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`🟢 Servidor corriendo en puerto ${PORT}`);
-});
+app.listen(process.env.PORT || 3000, () =>
+    console.log("Servidor andando en puerto 3000")
+);
