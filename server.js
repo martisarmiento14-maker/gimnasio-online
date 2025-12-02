3e72ff2df51e08b8c896afc0918ad44806f14e3a
martisarmiento14-maker <TU_EMAIL_DE_GITHUB>
2025-12-02 12:24:54 -0300

    Actualizo cambios del proyecto

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ import express from "express";
 import cors from "cors";
 import adminRoutes from "./routes/admin.js";
 import alumnosRoutes from "./routes/alumnos.js";
+import asistenciasRoutes from "./routes/asistencias.js";
+import loginRoutes from "./routes/login.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// RUTAS PRINCIPALES
+app.use("/login", loginRoutes);
 app.use("/admin", adminRoutes);
 app.use("/alumnos", alumnosRoutes);
+app.use("/asistencias", asistenciasRoutes);
 
-app.listen(process.env.PORT || 3000, () =>
-    console.log("Servidor andando en puerto 3000")
-);
+// SERVIDOR
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor funcionando en puerto 3000");
+});
