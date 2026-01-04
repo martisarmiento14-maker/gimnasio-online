6babe70496710dce3170f5717166dca10d71ec32
martisarmiento14-maker <TU_EMAIL_DE_GITHUB>
2026-01-04 20:51:26 -0300

    efk

---
+++ routes/pagos.js
@@ -0,0 +1,38 @@
+import express from "express";
+import db from "../database/db.js";
+
+const router = express.Router();
+
+// REGISTRAR PAGO
+router.post("/", async (req, res) => {
+    try {
+        const {
+            alumno_id,
+            monto,
+            metodo_pago,
+            tipo
+        } = req.body;
+
+    const query = `
+        INSERT INTO pagos 
+        (alumno_id, monto, metodo_pago, fecha_pago, tipo)
+        VALUES ($1, $2, $3, CURRENT_DATE, $4)
+        RETURNING *
+    `;
+
+    const result = await db.query(query, [
+        alumno_id,
+        monto,
+        metodo_pago,
+        tipo
+    ]);
+
+    res.json(result.rows[0]);
+
+    } catch (error) {
+    console.error("ERROR PAGO:", error);
+    res.status(500).json({ error: "Error registrando pago" });
+    }
+});
+
+export default router;
