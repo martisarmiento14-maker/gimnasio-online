a0a41d81c0ad330eb5f0cc3c0daf9fe9293176a0
martisarmiento14-maker <TU_EMAIL_DE_GITHUB>
2026-01-05 20:01:57 -0300

    efk

--- routes/pagos.js
+++ routes/pagos.js
@@ -10,28 +10,40 @@ router.post("/", async (req, res) => {
             id_alumno,
             monto,
             metodo_pago,
-            tipo
+            tipo,
+            plan,
+            dias_por_semana
         } = req.body;
 
-    const query = `
-        INSERT INTO pagos 
-        (id_alumno, monto, metodo_pago, fecha_pago, tipo)
-        VALUES ($1, $2, $3, CURRENT_DATE, $4)
-        RETURNING *
-    `;
+        const query = `
+            INSERT INTO pagos 
+            (
+                id_alumno,
+                monto,
+                metodo_pago,
+                fecha_pago,
+                tipo,
+                plan,
+                dias_por_semana
+            )
+            VALUES ($1, $2, $3, CURRENT_DATE, $4, $5, $6)
+            RETURNING *
+        `;
 
-    const result = await db.query(query, [
-        id_alumno,
-        monto,
-        metodo_pago,
-        tipo
-    ]);
+        const result = await db.query(query, [
+            id_alumno,
+            monto,
+            metodo_pago,
+            tipo,
+            plan,
+            dias_por_semana
+        ]);
 
-    res.json(result.rows[0]);
+        res.json(result.rows[0]);
 
     } catch (error) {
-    console.error("ERROR PAGO:", error);
-    res.status(500).json({ error: "Error registrando pago" });
+        console.error("ERROR PAGO:", error);
+        res.status(500).json({ error: "Error registrando pago" });
     }
 });
 
