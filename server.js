64fad12153326ea7e2da6fe33ff014c903017cda
martisarmiento14-maker <TU_EMAIL_DE_GITHUB>
2026-01-05 16:06:13 -0300

    efk

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-
 import adminRoutes from "./routes/admin.js";
 import alumnosRoutes from "./routes/alumnos.js";
 import asistenciasRoutes from "./routes/asistencias.js";
@@ -13,13 +12,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// RUTAS PRINCIPALES
 app.use("/login", loginRoutes);
 app.use("/admin", adminRoutes);
 app.use("/alumnos", alumnosRoutes);
 app.use("/asistencias", asistenciasRoutes);
 app.use("/pagos", pagosRoutes);
+
 app.use("/estadisticas", estadisticasRoutes);
 
+
+
+// SERVIDOR
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Servidor funcionando");
+    console.log("Servidor funcionando en puerto 3000");
 });
